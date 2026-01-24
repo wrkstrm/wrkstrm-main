4ea25055b601623de2beb0297f74304f8a784c01
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2026-01-23 21:05:26 -0800

    Revert "Update Package.swift"
    This reverts commit 68d81e64ee87186f781e7a700024f6130d16dca9.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 // MARK: - Package Declaration
 
 let package = Package(
-  name: "wrkstrm-main",
+  name: "WrkstrmMain",
   platforms: [
     .iOS(.v16),
     .macOS(.v13),
